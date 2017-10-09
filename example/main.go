8a53fe2071e979291c48fc4b2e36ce12b3b893da
Faisal Iqbal <faisal@interactiveblue.com>
2017-10-08 23:16:05 -0500

    Added Mongo

--- example/main.go
+++ example/main.go
@@ -4,14 +4,29 @@ import (
 	"fmt"
 
 	"github.com/sfi2k7/picoweb"
+	"gopkg.in/mgo.v2/bson"
 )
 
 func main() {
 	p := picoweb.New()
 	p.StopOnInt()
 	p.Get("/", func(c *picoweb.Context) {
-		c.View("./index.html", struct{ Name string }{Name: "Faisal"})
-		//c.String("Hello World")
+		s, err := c.Mongo()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer s.Close()
+		var all []bson.M
+		s.DB("local").C("startup_log").Find(bson.M{}).All(&all)
+
+		c.View("./index.html", struct {
+			Name string
+			All  interface{}
+		}{
+			All:  all,
+			Name: "Faisal",
+		})
 	})
 	fmt.Println(p.Listen(57432))
 }
