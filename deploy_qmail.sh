8ce2716d4e26595c4f77244b581ed8e1adba6f70
Faisal Iqbal <sfi2k7@users.noreply.github.com>
2022-12-12 01:28:47 +0500

    Delete deploy_qmail.sh

--- deploy_qmail.sh
+++
@@ -1,2 +0,0 @@
-#rsync -azv --exclude 'jobs'  --exclude '.git' --delete . root@bmysql:/root/gowork/src/github.com/faisal/blueq
-rsync -azv --exclude=blueregister  --exclude 'jobs' --exclude=*.exe  --exclude '.git' --delete . ubuntu@qmailserver:/home/ubuntu/go/src/github.com/sfi2k7/picoweb
